674b038fe8eec10123c0c24eec7b414122fdd505
aaaaruixxsrt-svg <aaaaruixxsrt@gmail.com>
2026-03-23 20:54:14 -0400

    Add Report Card Printer with student details

---
+++ python-basics/Report-Card-Printer.py
@@ -0,0 +1,17 @@
+name = 'Carter'
+print(name, type(name))
+
+
+is_student = True
+print(is_student, type(is_student))
+
+
+age = 20
+print(age, type(age))
+
+
+score = 80.5
+print(isinstance(score, float))
+print(score,type(score))
+
+
